7291d608028b2996037c14960b59ecec4af792df
Mateusz <mateusz.maciej@gmail.com>
2013-10-12 21:39:15 +0200

    GUI pre-alpha ver 0.00001+i codename: "But it's working"

--- src/main/java/org/aldeon/app/Main.java
+++ src/main/java/org/aldeon/app/Main.java
@@ -1,17 +1,22 @@
 package org.aldeon.app;
 
+import javafx.application.Application;
+import javafx.stage.Stage;
 import org.aldeon.common.EndpointWithPortPolicy;
 import org.aldeon.common.net.ConcretePort;
 import org.aldeon.common.net.StaticPortPolicy;
+import org.aldeon.gui.GUIManager;
 import org.aldeon.jetty.JettyModule;
 
 import java.io.IOException;
 
-public class Main {
+//javafx odpala tego starta przez czary i magię więc dopóki nie wykminię skąd on bierze
+//tego stage'a to tak będzie na tym branchu
+public class Main extends Application{
 
     public static void main(String[] args) {
-
-        EndpointWithPortPolicy endpoint = JettyModule.getEndpoint();
+        launch(args);
+        /*EndpointWithPortPolicy endpoint = JettyModule.getEndpoint();
         endpoint.setObserver(new FooObserver());
         endpoint.setPortPolicy(new StaticPortPolicy(new ConcretePort(8080), new ConcretePort(8080)));
         endpoint.start();
@@ -20,7 +25,13 @@ public class Main {
             System.in.read();
         } catch (IOException e) {}
 
-        endpoint.stop();
+        endpoint.stop();*/
     }
 
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        GUIManager gui = new GUIManager();
+        gui.start(stage);
+    }
 }
