098a359df256c42ba3acfa57144aba31c9961b68
Mateusz <mateusz.maciej@gmail.com>
2013-11-06 17:11:47 +0100

    Gui v0.2, changed to fxml, added css and controllers.

---
+++ src/main/java/org/aldeon/gui/GUIController.java
@@ -0,0 +1,11 @@
+package org.aldeon.gui;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Prophet
+ * Date: 06.11.13
+ * Time: 16:43
+ * To change this template use File | Settings | File Templates.
+ */
+public class GUIController {
+}
---
+++ src/main/java/org/aldeon/gui/controllers/ThreadController.java
@@ -0,0 +1,11 @@
+package org.aldeon.gui.controllers;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Prophet
+ * Date: 06.11.13
+ * Time: 16:39
+ * To change this template use File | Settings | File Templates.
+ */
+public class ThreadController {
+}
