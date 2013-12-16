fb5791829434c1b0af4d70f45879f43bb6b849f0
Michał Jabczyński <michal@jabczynski.net>
2013-12-16 12:56:37 +0100

    Started working on a Crawler module

---
+++ src/main/java/org/aldeon/dht/crawler/Crawler.java
@@ -0,0 +1,13 @@
+package org.aldeon.dht.crawler;
+
+/**
+ * Constantly communicates with other peers in order to
+ * broaden our knowledge about the network structure.
+ *
+ * Tasks:
+ *  - find peers with ids closest to our address hash (hashes).
+ *  - fulfill non-zero demands for peers interested in particular topics
+ *  - remove unresponsive peers from our dht structures
+ */
+public class Crawler {
+}
