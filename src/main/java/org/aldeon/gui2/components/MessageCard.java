165fdc251698fdfca6ceccadb8f6e15edcd520d2
Michał Jabczyński <michal@jabczynski.net>
2013-12-31 01:53:41 +0100

    MessageContent removed

--- src/main/java/org/aldeon/gui2/components/MessageCard.java
+++ src/main/java/org/aldeon/gui2/components/MessageCard.java
@@ -4,14 +4,22 @@ import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import org.aldeon.gui2.Gui2Utils;
 import org.aldeon.gui2.various.DeterministicColorGenerator;
 import org.aldeon.model.Message;
 
 public class MessageCard extends HorizontalColorContainer {
 
-    @FXML protected MessageContent messageData;
+    @FXML protected Label messageContentLabel;
+    @FXML protected Label messageIdLabel;
+    @FXML protected Label userNameLabel;
+    @FXML protected Label userIdLabel;
+    @FXML protected Button responseButton;
 
     private final ObjectProperty<Message> messageProperty = new SimpleObjectProperty<>();
 
@@ -21,13 +29,17 @@ public class MessageCard extends HorizontalColorContainer {
         messageProperty().addListener(new ChangeListener<Message>() {
             @Override
             public void changed(ObservableValue<? extends Message> observableValue, Message oldMessage, Message newMessage) {
-                messageData.setAuthorName("Anonymous");
-                messageData.setAuthorHash(newMessage.getAuthorPublicKey().toString());
-                messageData.setMessageHash(newMessage.getIdentifier().toString());
-                messageData.setText(newMessage.getContent());
+                userNameLabel.setText("Anonymous");
+                userIdLabel.setText(newMessage.getAuthorPublicKey().toString());
+                messageIdLabel.setText(newMessage.getIdentifier().toString());
+                messageContentLabel.setText(newMessage.getContent());
                 setColor(DeterministicColorGenerator.get(newMessage.getAuthorPublicKey().hashCode()));
             }
         });
+    }
+
+    @FXML protected void onLink(ActionEvent event) {
+        Gui2Utils.copyToClipboard(messageIdLabel.getText());
     }
 
     public MessageCard(Message message) {
@@ -45,5 +57,17 @@ public class MessageCard extends HorizontalColorContainer {
 
     public void setMessage(Message message) {
         messageProperty().set(message);
+    }
+
+    public ObjectProperty<EventHandler<ActionEvent>> onResponseProperty() {
+        return responseButton.onActionProperty();
+    }
+
+    public EventHandler<ActionEvent> getOnResponse() {
+        return onResponseProperty().get();
+    }
+
+    public void setOnResponse(EventHandler<ActionEvent> onResponse) {
+        onResponseProperty().set(onResponse);
     }
 }
