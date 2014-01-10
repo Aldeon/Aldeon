f0359d7d0d29e594f01bb77f6a0b2f38001b64c8
Michał Jabczyński <michal@jabczynski.net>
2014-01-10 10:55:02 +0100

    After each successful GetRelevantPeers, an IndicateInterest request is sent

--- src/main/java/org/aldeon/dht/crawler/IndicateInterestTask.java
+++ src/main/java/org/aldeon/dht/crawler/IndicateInterestTask.java
@@ -1,5 +1,6 @@
 package org.aldeon.dht.crawler;
 
+import org.aldeon.events.Callback;
 import org.aldeon.model.Identifier;
 import org.aldeon.networking.common.OutboundRequestTask;
 import org.aldeon.networking.common.PeerAddress;
@@ -7,28 +8,28 @@ import org.aldeon.protocol.Request;
 import org.aldeon.protocol.Response;
 import org.aldeon.protocol.request.GetRelevantPeersRequest;
 import org.aldeon.protocol.request.IndicateInterestRequest;
+import org.aldeon.protocol.response.AddressSavedResponse;
 import org.aldeon.sync.tasks.AbstractOutboundTask;
 
-public class IndicateInterestTask extends AbstractOutboundTask<GetRelevantPeersRequest> {
+public class IndicateInterestTask extends AbstractOutboundTask<IndicateInterestRequest> {
 
-    private final PeerAddress peer;
-    private final Identifier topic;
-    private final PeerAddress myself;
+    private final Callback<Boolean> onFinished;
 
-    public IndicateInterestTask(Identifier topic, PeerAddress peer, PeerAddress myself) {
-        super(null);
-        this.topic = topic;
-        this.peer = peer;
-        this.myself = myself;
+    public IndicateInterestTask(Identifier topic, PeerAddress peer, PeerAddress myself, Callback<Boolean> onFinished) {
+        super(peer);
+        setRequest(new IndicateInterestRequest());
+        getRequest().topic = topic;
+        getRequest().address = myself;
+        this.onFinished = onFinished;
     }
 
     @Override
     public void onSuccess(Response response) {
-
+        onFinished.call(response instanceof AddressSavedResponse);
     }
 
     @Override
     public void onFailure(Throwable cause) {
-
+        onFinished.call(false);
     }
 }
