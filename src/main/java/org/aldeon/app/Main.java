7f8660e804beb3dacce7435cec94dd7d1a03629c
Łukasz Inatlewski <lukasz.inatlewski@gmail.com>
2014-01-16 01:55:38 +0100

    Single instance of application verification, Database connection second chance, deleting lock file, Gradle build file cleaning

--- src/main/java/org/aldeon/app/Main.java
+++ src/main/java/org/aldeon/app/Main.java
@@ -7,13 +7,23 @@ import org.aldeon.gui2.Gui2Module;
 import org.aldeon.model.Message;
 import org.aldeon.protocol.ProtocolModule;
 import org.aldeon.utils.conversion.ConversionException;
+import org.aldeon.utils.various.VirtualMachine;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.util.Set;
 
 public class Main {
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) throws IOException, ConversionException {
+        if(VirtualMachine.getNumberOfApplicationInstances() > 1)
+        {
+            log.error("Attempting to run more than one instance. Exiting.");
+            System.exit(-1);
+        }
+
         ProtocolModule.initialize();
         Gui2Module.launch();
     }
